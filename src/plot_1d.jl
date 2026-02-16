af9d4372635748527291cc78305da4b642e58546
David Gómez-Castro <david@gomezcastro.xyz>
2026-02-16 07:34:08 +0100

    Add plot_1d! function for 1D plotting

--- src/plot_1d.jl
+++ src/plot_1d.jl
@@ -25,3 +25,29 @@ function plot_1d(ρ, mesh; color=nothing, alpha=1.0, label="")
 
     return p
 end
+
+function plot_1d!(ρ, mesh; color=nothing, alpha=1.0, label="")
+    d = dimension(mesh)
+    if d ≠ 1
+        @error "This function is for d=1"
+    end
+
+    x = [FVADE.x(i, mesh.h)[1] for i in mesh.Ih]
+
+    if isnothing(color)
+        p = plot!(
+            x, ρ,
+            label=label,
+            alpha=alpha
+        )
+    else
+        p = plot!(
+            x, ρ,
+            label=label,
+            color=color,
+            alpha=alpha
+        )
+    end
+
+    return p
+end
